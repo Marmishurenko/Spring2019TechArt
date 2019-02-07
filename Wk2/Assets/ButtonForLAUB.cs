49236517f894cb6b1f0f1d9772ef14bbc4fa7dd9
marmishurenko <marmishurenko@gmail.com>
2019-02-06 20:11:47 -0500

    added terrain and textures, basic camera works

---
+++ Wk2/Assets/ButtonForLAUB.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+[CustomEditor(typeof(LookAtUnityBezier))]
+
+    public class ButtonsForLAUB : Editor
+    {
+
+        public override void OnInspectorGUI()
+        {
+        LookAtUnityBezier myLAUB = (LookAtUnityBezier)target; //casting class into object
+        DrawDefaultInspector();
+            if (GUILayout.Button("Make new curve"))
+            {
+            BezierExample newBE = myLAUB.myModel.gameObject.AddComponent<BezierExample>();
+            if (myLAUB.curveList.Count > 0)
+            {
+                BezierExample lastBE = myLAUB.curveList[myLAUB.curveList.Count - 1];
+                newBE.startPoint = lastBE.endPoint;
+                newBE.endPoint = lastBE.endPoint;
+                newBE.startTangent = lastBE.endPoint;
+                newBE.endTangent = lastBE.endPoint;
+            }
+            
+            myLAUB.curveList.Add(newBE);
+            }
+        }
+    }
+
+
---
+++ Wk2/Assets/LookAtUnityBezier.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+//[ExecuteInEditMode] // use sparingly
+
+public class LookAtUnityBezier : MonoBehaviour {
+
+    public GameObject marker;
+    public BezierExample bezEx;
+    public List<BezierExample> curveList = new List<BezierExample>();
+    public Transform myModel;
+
+    public void Start()
+    {
+        PutPointsOnCurve();
+    }
+
+    private void PutPointsOnCurve()
+    {
+        //Run though 100 points and place a marker at those points on the bezier curve
+        //for loop through 100 points  btw 0 and 1 
+        //pass that fraction to a curve calculator to find the resultant v3
+        //Place a marker at that v3
+
+        for (int i = 0; i <= 100; i++)
+        {
+            float t = (float)i / 100;
+            Vector3 pointOnCurve = CalculateBezier(bezEx, t);
+            Instantiate(marker, pointOnCurve, Quaternion.identity, null);
+        }
+    }
+
+    // Use this for initialization
+    void Update () {
+       
+        
+	}
+
+    Vector3 CalculateBezier(BezierExample curvData, float t)
+    {
+        Vector3 a= curvData.startPoint;
+        Vector3 b= curvData.startTangent;
+        Vector3 c= curvData.endPoint;
+        Vector3 d= curvData.endTangent;
+
+        Vector3 ab = Vector3.Lerp(a, b, t);
+        Vector3 bc = Vector3.Lerp(b, c, t);
+        Vector3 cd = Vector3.Lerp(c, d, t);
+        
+        Vector3 abc = Vector3.Lerp(ab, bc, t);
+        Vector3 bcd = Vector3.Lerp(bc,cd, t);
+
+        Vector3 final = Vector3.Lerp(abc, bcd, t);
+
+        return final;
+    }
+
+
+
+
+}
